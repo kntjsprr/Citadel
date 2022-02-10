42d4674e38ce2b2f1c62ffd3138eb22ef2c13964
000 <65537922+kntjspr@users.noreply.github.com>
2022-02-10 20:07:57 +0800

    Update Program.cs
    Fixed some jargon statements.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
@@ -11,30 +11,24 @@ namespace Citadel
     {
         static async Task Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
 
             #region Initializing Variables
-
             string encryptionKey, extension;
             byte[] encryptionKeyBytes;
             CitadelMain.Mode conversionMode;
-            string defaultKey = "citadel0000Encrypt#!%@#";
-
+            string defaultKey = "citadel0000Encrypt#!%@#"; //Change this value to change the defaultKey.
             #endregion
-            Console.ForegroundColor = ConsoleColor.Green;
 
              /*       Other variable used      
              * filePath and fileName used to split fullPath (Currently not used. Initially created to implement predetermined filenames.)
-             */
+             * Length starts with 1 but array start with 0  */
 
-
-            //Length starts with 1 but array start with 0
-
-            if (args.Length <= 0) //No argument
-            {
+            if (args.Length <= 0) //No argument {
                 Console.WriteLine("No arguments are entered. Use -h for help.");
                 return;
             } else if (args[0] == "-h") {
-                Console.WriteLine("\nShowing Help:");
+                Console.WriteLine("\nShowing Help (1/1): ");
                 Console.WriteLine("=======================\n");
                 Console.WriteLine("Args Guidelines:\n\n 1st: [the path of the file you wish to modify] \n 2nd: [folder where your file will be saved] \n 3rd: [-e to encrypt/-d to decrypt] \n 4th: [encryption key (optional)] \n 5th: [file extension (optional)]\n\n");
                 Console.WriteLine("Example args : Citadel.exe \"%temp%\\filetomodify.exe %temp%\\FolderToSave\" -e  secretpasswordencr33 exe");
@@ -51,7 +45,7 @@ namespace Citadel
                 return;
             }
 
-            if (args.Length < 2){ //If equl or more than 0 which means it only contains first argument args[0]
+            if (args.Length < 2){ 
                 Console.WriteLine("No path to save. Use -h to help");
                 return;
             } else if (args.Length < 3) { 
@@ -60,28 +54,22 @@ namespace Citadel
             }
 
             /* Since the args are not empty. Let's assign them and check if they are valid.*/
-        
 
             string fullPath = @args[0];
             string savePath = @args[1]; 
             string action = @args[2];
 
-
-
-
-
             if (!File.Exists(fullPath))
             {
-                Console.WriteLine("Argument 1 " + fullPath + " doesn't exist. \n" +
+                Console.WriteLine("File " + fullPath + " doesn't exist. \n" +
                                               "Use -h for help.");
                 return;
             }
             else if (!Directory.Exists(savePath))
             {
-                Console.Write("Argument 2 " + savePath + "doesn't exist. \nUse -h for help.");
+                Console.Write("Directory " + savePath + "doesn't exist. \nUse -h for help.");
                 return;
             }
-
             switch (action)
             {
                 case "-e":
@@ -91,15 +79,13 @@ namespace Citadel
                conversionMode = CitadelMain.Mode.Restore;
                     break;
                 default:
-                    Console.WriteLine("Invalid third argument. Use -h for help.");
+                    Console.WriteLine("Invalid action. Use -h for help.");
                     return;
             }
-           
             if (args.Length < 3)
             {
                 encryptionKey = defaultKey; 
-            } else if(args[3] == "")
-            {
+            } else if(args[3] == ""){
                 encryptionKey = defaultKey; 
             } else {
                 encryptionKey = args[3];
@@ -111,13 +97,12 @@ namespace Citadel
                 extension = @args[4];
             }
 
-            
+           /* Separate full path into a directory and file name. Not used/referenced at this moment*/
             Regex rxPattern = new Regex(@"[^\\\\]*$"); // Regex pattern to use
             Match rx = rxPattern.Match(fullPath); //Match fileName pattern to fullpath and put it into a variable named fileName
             string filePath = Regex.Replace(fullPath, rx.Value, ""); //Remove fileName value and name it to a var named filePath
             string fileName = rx.Value;
 
-         
             using (SHA256Managed SHA256 = new SHA256Managed())
                 encryptionKeyBytes = SHA256.ComputeHash(Encoding.UTF8.GetBytes(encryptionKey));
             Task<string> task;
@@ -127,20 +112,11 @@ namespace Citadel
             if (conversionMode == CitadelMain.Mode.Transform)
             {
                 task = Task.Run(() => ft.TransformFile(fullPath, savePath, extension));
-            }
-            else
-            {  // (conversionMode == CitadelMain.Mode.Restore)
+            } else {  // (conversionMode == CitadelMain.Mode.Restore)
                 task = Task.Run(() => ft.RestoreFile(fullPath, savePath));
-
             }
             newFilePath = await task;
             Console.Write(Path.GetFileName(newFilePath));
-
-
-
         }
-
-
         }
-
     }
