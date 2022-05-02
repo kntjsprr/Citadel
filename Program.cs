64b1167afe27cf3877546a7f648c81bb2374848a
000 <65537922+kntjspr@users.noreply.github.com>
2022-05-02 20:52:44 +0800

    Add files via upload
    - Fixed an error in array counting that results in array out of bounds error.
    - Optimized code a lot

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-using System;
+﻿using System;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace Citadel
 {
@@ -11,89 +10,83 @@ namespace Citadel
     {
         static async Task Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-
             #region Initializing Variables
-            string encryptionKey, extension;
+
+            string encryptionKey, extension, fullPath, savePath, action;
             byte[] encryptionKeyBytes;
             CitadelMain.Mode conversionMode;
-            string defaultKey = "y2J7gj+=fkN-g&h7"; 
+            string defaultKey = @"LLp53sQt%BDPmKn?";
+            // filePath and fileName used to split fullPath (Currently not used. Initially created to implement predetermined filenames.)
             #endregion
-            Console.WriteLine("Citadel v1.0.4");
-            Console.WriteLine("https://github.com/kntjspr/Citadel");
-             /*       Other variable used      
-             * filePath and fileName used to split fullPath (Currently not used. Initially created to implement predetermined filenames.)
-             * Length starts with 1 but array start with 0  */
 
-            if (args.Length <= 0) {//No argument
+
+            int pointer = args.Length - 1;
+            if (pointer < 0) //No argument
+            {
                 Console.WriteLine("No arguments are entered. Use -h for help.");
                 return;
             } else if (args[0] == "-h") {
-                Console.WriteLine("\nShowing Help (1/1): ");
+                Console.WriteLine("\nShowing Help:");
                 Console.WriteLine("=======================\n");
-                Console.WriteLine("Args: [the path of the file you wish to modify] [folder where your file will be saved] [-e to encrypt/-d to decrypt]  [encryption key (optional)]  [file extension (optional)]\n\n");
-                Console.WriteLine("Example args : Citadel.exe \"%temp%\\filetomodify.exe %temp%\\FolderToSave\" -e  secretpasswordencr33 exe");
-                Console.WriteLine("This one uses the full args, password set to \"secretpasswordencr33\" and extension to \".exe\"");
-                Console.WriteLine("https://github.com/kntjspr/Citadel/blob/main/README.md");
+                Console.WriteLine("Args : [the path of the file you wish to modify]  [folder where your file will be saved] [-e to encrypt/-d to decrypt] 4th: [encryption key (optional)]  5th: [file extension (optional)]\n\n");
+
                 return;
             }
 
-            if (args.Length < 2){ 
+            if (pointer < 1){
                 Console.WriteLine("No path to save. Use -h to help");
                 return;
-            } else if (args.Length < 3) { 
+            } else if (pointer < 2) {
                 Console.WriteLine("No command specified whether to encrypt or decrypt. Use -h for help.");
                 return;
             }
-
-            string fullPath = @args[0];
-            string savePath = @args[1]; 
-            string action = @args[2];
+           fullPath = @args[0]; savePath = @args[1]; action = @args[2];
 
             if (!File.Exists(fullPath))
             {
-                Console.WriteLine("File " + fullPath + " doesn't exist. \n" +
+                Console.WriteLine("Argument " + fullPath + " doesn't exist. \n" +
                                               "Use -h for help.");
                 return;
             }
             else if (!Directory.Exists(savePath))
             {
-                Console.Write("Directory " + savePath + "doesn't exist. \nUse -h for help.");
+                Console.Write("Argument " + savePath + "doesn't exist. \nUse -h for help.");
                 return;
             }
             switch (action)
             {
-                case "-e":
+                case @"-e":
               conversionMode = CitadelMain.Mode.Transform;
                     break;
-                case "-d":
+                case @"-d":
                conversionMode = CitadelMain.Mode.Restore;
                     break;
                 default:
-                    Console.WriteLine("Invalid action. Use -h for help.");
+                    Console.WriteLine("Invalid argument. Use -h for help.");
                     return;
             }
-            if (args.Length < 3)
+                if (pointer < 3)
             {
-                encryptionKey = defaultKey; 
-            } else if(args[3] == ""){
-                encryptionKey = defaultKey; 
+                encryptionKey = defaultKey;
+            }
+                 else if(args[3] == "")
+            {
+                encryptionKey = defaultKey;
             } else {
                 encryptionKey = args[3];
-            }
-            
-            if (args.Length < 4){
-                extension = "enc"; 
-            } else {
+            } if (pointer < 4)
+            {
+                extension = "enc"; //File extension of the encrypted file
+            }  else {
                 extension = @args[4];
             }
 
-           /* Separate full path into a directory and file name. Not used/referenced at this moment
-           *
-           * Regex rxPattern = new Regex(@"[^\\\\]*$"); // Regex pattern to use
-           * Match rx = rxPattern.Match(fullPath); //Match fileName pattern to fullpath and put it into a variable named fileName
-           * string filePath = Regex.Replace(fullPath, rx.Value, ""); //Remove fileName value and name it to a var named filePath
-           * string fileName = rx.Value; */
+            /*
+            Regex rxPattern = new Regex(@"[^\\\\]*$"); // Regex pattern to use
+            Match rx = rxPattern.Match(fullPath); //Match fileName pattern to fullpath and put it into a variable named fileName
+            string filePath = Regex.Replace(fullPath, rx.Value, ""); //Remove fileName value and name it to a var named filePath
+            string fileName = rx.Value;
+            */
 
             using (SHA256Managed SHA256 = new SHA256Managed())
                 encryptionKeyBytes = SHA256.ComputeHash(Encoding.UTF8.GetBytes(encryptionKey));
@@ -104,11 +97,20 @@ namespace Citadel
             if (conversionMode == CitadelMain.Mode.Transform)
             {
                 task = Task.Run(() => ft.TransformFile(fullPath, savePath, extension));
-            } else {  // (conversionMode == CitadelMain.Mode.Restore)
+            }
+            else
+            {  // (conversionMode == CitadelMain.Mode.Restore)
                 task = Task.Run(() => ft.RestoreFile(fullPath, savePath));
+
             }
             newFilePath = await task;
-            Console.Write(Path.GetFileName(newFilePath));
+            Console.Write("Output: " + savePath + @"\" + Path.GetFileName(newFilePath));
+
+
+
         }
+
+
         }
+
     }
--- README.md
+++ README.md
@@ -1,56 +1,37 @@
 # Citadel
-A simple command-line utility for encrypting files using the AES-256-GCM algorithm and then converting them into a base64 format. 
+A simple command-line utility for encrypting files with the AES-256-GCM algorithm and then converting them to base64 format. 
 
-## Features
-- Encrypt any file using the AES-256-GCM algorithm and then encoding it into a base64 format.
+# Features
+- Encrypt any file using AES-256-GCM algorithm and then encoding it into a base64 format.
 - The original filename is included in the encrypted file and may be changed without affecting the original file. 
 - Everything can be done using args! You can integrate this using your own app easily using cmd.
 
-## File structure
-- Chunk size can be modified on ```CitadelMain.cs```
-
-![File Structure](https://raw.githubusercontent.com/kntjspr/Citadel/main/Github/file-structure.png)
-
 
 ## How to use
-This tool doesn't need any user interaction on the CLI. You'll just have to pass the arguments on the exe file.
+This tool doesn't need any user interaction on the CLI (like console.read). You'll just have to pass the arguments on the exe file.
 
 ## Arguments
 
-Citadel.exe [path of the file you wish to encrypt/decrypt]  [path to the directory in which the result will be saved] [-e/-d (encrypt/decrypt)] [password (optional)] [File Extension (Optional)] 
+Citadel.exe [file you wish to encrypt/decrypt]  [path to the directory in which the result will be saved] [-e/-d (encrypt/decrypt)] [password (optional)] [extension file (optional)]
 
-- File Extension argument are ignored for -d/decrypt args since [the original filename will be used](https://github.com/kntjspr/Citadel#features)
+Example:
+Citadel.exe "%temp%\\file.exe" "%temp%" -e password7737 encfile
 
-### Encrypting file example:
-``Citadel.exe C:\DecryptedFolder\file.exe C:\EncryptedFolder -e password7737 cit``
+The encrypted file will be stored as .encfile in the%temp% folder and will print the filename on the cli. 
 
-- Output sample: C:\EncryptedFolder\\[dbcsiwpqen.cit](https://github.com/kntjspr/Citadel#other-info)
-
-### Decrypting file example:
-``Citadel.exe C:\EncryptedFolder\dbcsiwpqen.cit C:\DecryptedFolder -d password7737``
-
-- Output sample: C:\DecryptedFolder\file.exe
-
-## Default values
+## Default Values
 The last two arguments are optional.
 If they are left blank, the pre-defined default values will be used. 
 
 Password = "citadel0000Encrypt#!%@#"
 
-File Extension = ".enc"
+Extension file = ".enc"
+
+## Other Info
+- File names are randomized and they will be printed on the cli. I might add a feature to use a predetermined file names.
 
 ## Screenshot
 ![Screenshot](https://raw.githubusercontent.com/kntjspr/Citadel/main/Github/Screenshot%202022-02-10%20073809.png)
-
-## Other info
-- File names are randomized when you encrypt the files to avoid file overwrite. I might add a feature to use a predetermined file names.
-
-## Features to add
-- Option to choose your own encrypted output filename and file extension
-- Ability to select multiple files/folder to encrypt at once. 
-
-(You can choose to encrypt them one by one or to repack them into a one zip file).
-
 ## Troubleshoot
 For runtime problems install [.NET 5.0 Runtime](https://dotnet.microsoft.com/en-us/download/dotnet/5.0) 
 and if you want to modify the source you must install .NET 5.0 SDK
